9a19eb7799c239e35eedf93dff6970ae85f05e0a
shirinfathima <shirinfathima003@gmail.com>
2026-05-04 21:00:35 +0530

    docker

--- Trust-net-main/ai-microservices/Face-match/app.py
+++ Trust-net-main/ai-microservices/Face-match/app.py
@@ -85,5 +85,5 @@ def face_match():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
